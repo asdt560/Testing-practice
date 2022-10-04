6e8110667a9398fc3091d5b3ccb13b4d05f4735f
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-10-04 13:21:22 -0300

    Add calculator class and tests

---
+++ calculator.js
@@ -0,0 +1,16 @@
+class calculator {
+  sum (num1, num2) {
+    return num1 + num2;
+  };
+  substract (num1, num2) {
+    return num1 - num2;
+  };
+  multiply (num1, num2) {
+    return num1 * num2;
+  };
+  divide (num1, num2) {
+    return num1 / num2;
+  };
+}
+
+module.exports = calculator;
--- functions.test.js
+++ functions.test.js
@@ -26,3 +26,65 @@ it('reverse check', () => {
 });
 
 /* Tests for Calculator */
+
+const calculator = require ('./calculator');
+
+describe('calculator', () => {
+  const calc = new calculator();
+
+  describe('calculator sum', () => {
+    it('sum positive', () => {
+      expect(calc.sum(1, 2)).toBe(3);
+    });
+
+    it('sum negative', () => {
+      expect(calc.sum(-1, -2)).toBe(-3);
+    });
+
+    it('sum mixed', () => {
+      expect(calc.sum(300, -533)).toBe(-233);
+    });
+  });
+
+  describe('calculator substract', () => {
+    it('substract positive', () => {
+      expect(calc.substract(5, 3)).toBe(2);
+    });
+
+    it('substract negative', () => {
+      expect(calc.substract(-20, -40)).toBe(20);
+    });
+
+    it('substract mixed', () => {
+      expect(calc.substract(624, -305)).toBe(929);
+    });
+  });
+
+  describe('calculator multiply', () => {
+    it('multiply positive', () => {
+      expect(calc.multiply(5, 3)).toBe(15);
+    });
+
+    it('multiply negative', () => {
+      expect(calc.multiply(-9, -9)).toBe(81);
+    });
+
+    it('multiply mixed', () => {
+      expect(calc.multiply(5, -6)).toBe(-30);
+    });
+  });
+
+  describe('calculator divide', () => {
+    it('divide positive', () => {
+      expect(calc.divide(15, 3)).toBe(5);
+    });
+
+    it('divide negative', () => {
+      expect(calc.divide(-81, -9)).toBe(9);
+    });
+
+    it('divide mixed', () => {
+      expect(calc.divide(30, -6)).toBe(-5);
+    });
+  });
+});
